c2f388658332dac7a8a07e58f632e46ac7cc6d8b
Cihan <ozcancihan714@gmail.com>
2023-06-23 14:10:11 +0300

    Item animation

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Item = ({content, percentage, voters}) => {
   return (
-    <div className="border border-blue my-3 w-1/2 flex justify-between items-center pr-3">
-      <div className={`bg-${percentage === 0 ? "white" : "blue"} text-${percentage === 0 ? "black" : "white"} w-[${percentage}%] h-full p-2`}>
+    <div className="border border-blue my-3 w-1/2 flex justify-between items-center pr-3 overflow-hidden">
+      <div className={`bg-${percentage === 0 ? "white" : "blue"} text-${percentage === 0 ? "black" : "white"} w-[${percentage}%] h-full p-2 slide-right`}>
         <span className="font-medium sm:text-base text-xs">{content}</span>
       </div>
 
