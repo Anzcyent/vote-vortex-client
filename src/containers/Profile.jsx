daeabf812fb1158d9f3c8ba263a6a5f5ec629794
Cihan <ozcancihan714@gmail.com>
2023-06-24 21:50:17 +0300

    User's surveys

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -3,11 +3,11 @@ import Surveys from "./Surveys";
 import { useSelector, useDispatch } from "react-redux";
 import { getUser } from "../redux/actions/user";
 import { useParams } from "react-router-dom";
+import { SurveyCard } from "../components";
 
 const Profile = () => {
   const { current_user } = useSelector((state) => state.userReducer);
   const { isLoaded } = useSelector((state) => state.appReducer);
-  console.log(current_user)
 
   const dispatch = useDispatch();
 
@@ -17,6 +17,13 @@ const Profile = () => {
     dispatch(getUser(id));
   }, []);
 
+  if (current_user?.surveys?.length === 0) {
+    <section className="grow font-poppins scale-up-center w-full flex__col-center">
+      <h3 className="font-bold text-green sm:text-2xl text-lg">
+        No survey found
+      </h3>
+    </section>;
+  }
 
   return (
     <section className="grow font-poppins w-full overflow-auto scroll-none">
@@ -26,7 +33,19 @@ const Profile = () => {
         </h1>
       </div>
 
-      <Surveys />
+      <section className="grow overflow-auto scroll-none font-poppins scale-up-center w-full">
+        <div className="container flex sm:flex-row flex-col items-center lg:justify-start justify-evenly flex-wrap">
+          {current_user?.surveys?.map((survey) => (
+            <SurveyCard
+              key={survey._id}
+              _id={survey._id}
+              title={survey.title}
+              description={survey.description}
+              username={current_user.username}
+            />
+          ))}
+        </div>
+      </section>
     </section>
   );
 };
